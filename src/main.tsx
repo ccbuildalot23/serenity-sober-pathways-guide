b7dea2aa03cc61e1620d2e6ddecbaead6ad75400
ccbuildalot23 <cmcald1018@gmail.com>
2025-06-15 20:22:06 -0400

    Initialize security before rendering

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from '@/components/theme/ThemeProvider';
+import { SecurityInitializer } from '@/lib/securityInitializer';
+
+await SecurityInitializer.initialize();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
